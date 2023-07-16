d8368542143520619c585d045e6537079139840e
elios11 <eliosferre3@hotmail.com>
2023-07-15 23:21:05 -0300

    Update modal template & close modal functionality

--- scripts/showCardData.js
+++ scripts/showCardData.js
@@ -8,6 +8,7 @@ const API_PATH = "https://api.rawg.io/api/";
 const MODAL = document.getElementById("gameModal");
 const MODAL_BODY = document.querySelector("#gameModal .gameModalBody");
 const MODAL_IMAGES = document.querySelector("#gameModal .gameImages");
+const CLOSE_MODAL_BTN = document.getElementById("closeModalBtn");
 
 function showGameInfo(gameData, gameScreenshots) {
     let platformsSvg = [];
@@ -31,8 +32,6 @@ function showGameInfo(gameData, gameScreenshots) {
         rgba(48, 48, 48, 1) 22.4rem
     ),
     url("${gameData.background_image}"`;
-    console.log(gameData);
-    console.log(gameScreenshots);
 
     if (gameData.genres) {
         gameData.genres.forEach((genre) => genres.push(genre.name));
@@ -79,73 +78,78 @@ function showGameInfo(gameData, gameScreenshots) {
         <p class="description">
             ${gameData.description_raw || "Game description was not found..."}
         </p>
-        <div class="add-to-wishlist-container">
-            <button>Add to wishlist</button>
-            <img src="img/components/heart.svg">
+        <div class="buttons-container">
+            <div class="add-to-wishlist-container">
+                <button>Add to wishlist</button>
+                <img src="img/components/heart.svg">
+            </div>
+            <button class="purchase-button">
+                Purchase
+            </button>
         </div>
-        <button class="purchase-button">
-            Purchase
-        </button>
-        <div class="game-property-container">
-            <p>Platforms</p>
-            <a href="#">${platforms.join(", ")}</a>
-        </div>
-        <div class="game-property-container">
-            <p>Release date</p>
-            <p>${releaseDate}</p>
-        </div>
-        <div class="game-property-container">
-            <p>Publisher</p>
-            <a href="#">
-                ${gameData.publishers[0]?.name || "No publishers found..."}
-            </a>
-        </div>
-        <div class="game-property-container">
-            <p>Website</p>
-            <a href="${gameData.website || "#"}">${gameData.website}</a>
-        </div>
-        <div class="game-property-container">
-            <p>Genre</p>
-            <a href="#">${genres.join(", ")}</a>
-        </div>
-        <div class="game-property-container">
-            <p>Developer</p>
-            <a href="#">${developers.join(", ")}</a>
-        </div>
-        <div class="game-property-container">
-            <p>Age rating</p>
-            <p>${gameData.esrb_rating?.name || "Not rated"}</p>
+        <div class="game-properties">
+            <div class="game-property-container">
+                <p>Platforms</p>
+                <a href="#">${platforms.join(", ")}</a>
+            </div>
+            <div class="game-property-container">
+                <p>Release date</p>
+                <p>${releaseDate}</p>
+            </div>
+            <div class="game-property-container">
+                <p>Publisher</p>
+                <a href="#">
+                    ${gameData.publishers[0]?.name || "No publishers found..."}
+                </a>
+            </div>
+            <div class="game-property-container">
+                <p>Website</p>
+                <a href="${gameData.website || "#"}">${gameData.website}</a>
+            </div>
+            <div class="game-property-container">
+                <p>Genre</p>
+                <a href="#">${genres.join(", ")}</a>
+            </div>
+            <div class="game-property-container">
+                <p>Developer</p>
+                <a href="#">${developers.join(", ")}</a>
+            </div>
+            <div class="game-property-container">
+                <p>Age rating</p>
+                <p>${gameData.esrb_rating?.name || "Not rated"}</p>
+            </div>
         </div>
         <div class="game-critics-container">
             <a><img src="img/components/chat-bubbles.svg"></a>
             <a><img src="img/components/thumbs-up.svg"></a>
             <a><img src="img/components/action.svg"></a>
         </div>
+
+        <div class="gameImages">
+            <div>
+                <img src="${images[0]}" alt="Background image 1 for
+                    ${gameData?.name}">
+                <img class="yt-play-btn" src="img/components/play-video-button.svg">
+            </div>
+            <div>
+                <img src="${images[1]}" alt="Background image 2 for
+                    ${gameData?.name}">
+                <img src="${images[2]}" alt="Background image 3 for
+                    ${gameData?.name}">
+            </div>
+            <div>
+                <img src="${images[3]}" alt="Background image 4 for
+                    ${gameData?.name}">
+                <img src="${images[4]}" alt="Background image 5 for
+                    ${gameData?.name}">
+            </div>
+        </div>
     `;
 
-    let gameImagesTemplate = `
-        <div>
-            <img src="${images[0]}" alt="Background image 1 for
-                ${gameData?.name}">
-            <img class="yt-play-btn" src="img/components/play-video-button.svg">
-        </div>
-        <div>
-            <img src="${images[1]}" alt="Background image 2 for 
-                ${gameData?.name}">
-            <img src="${images[2]}" alt="Background image 3 for 
-                ${gameData?.name}">
-        </div>
-        <div>
-            <img src="${images[3]}" alt="Background image 4 for 
-                ${gameData?.name}">
-            <img src="${images[4]}" alt="Background image 5 for 
-                ${gameData?.name}">
-        </div>
-    `;
-    MODAL_IMAGES.innerHTML = gameImagesTemplate;
     MODAL_BODY.innerHTML = modalTemplate;
 }
 
+/* Fetch data and display the modal */
 async function showModal(e) {
     const CARD_CONTAINER = e.target.closest(".game-card");
     if (!CARD_CONTAINER || !CARD_CONTAINER.dataset.id) {
@@ -165,15 +169,23 @@ async function showModal(e) {
     showGameInfo(data, screenshots);
     MODAL.showModal();
     showFullDescription();
-    document.body.style.overflow = "hidden";
+    document.body.classList.add("no-overflow");
     document.querySelector(".navbar").style.position = "sticky";
     document.querySelector(".navbar").style.zIndex = "999";
     document.querySelector(".navbar").style.top = "0";
 }
 document.addEventListener("click", showModal);
 
-function closeModal() {}
+/* Close game modal */
+MODAL.addEventListener("close", () => {
+    document.body.classList.remove("no-overflow");
+});
 
+CLOSE_MODAL_BTN.addEventListener("click", () => {
+    MODAL.close();
+});
+
+/* Expand description */
 function showFullDescription() {
     const MODAL_DESCRIPTION = document.querySelector("#gameModal .description");
 
