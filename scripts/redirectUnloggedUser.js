2170db18cf81941503e4c1a37bdf4b0d849d44ad
elios11 <eliosferre3@hotmail.com>
2023-07-31 16:02:39 -0300

    Fix bug where login page would reload indefinitely

--- scripts/redirectUnloggedUser.js
+++ scripts/redirectUnloggedUser.js
@@ -1,21 +1,20 @@
 import isLocalEnabled from "./localStorageEnabled.js";
 
-/* Redirects users to login page if they are not logged in, and to homepage if they are*/
+/* Redirects users to login page if they are not logged in, and to homepage if they are */
 (function () {
     "use strict";
-    if (
-        isLocalEnabled(
-            "Could not get access token due to local storage being disabled..."
-        )
-    ) {
-        if (localStorage.getItem("sessionId")) {
-            const ACCOUNT_MANAGEMENT_PATHS = [LOGIN_PATH, REGISTER_PATH];
+    /* prettier-ignore */
+    if (isLocalEnabled("Could not get access token due to local storage being disabled...")) {
+        const ACCOUNT_MANAGEMENT_PATHS = [LOGIN_PATH, REGISTER_PATH];
 
-            if (ACCOUNT_MANAGEMENT_PATHS.includes(window.location.href)) {
-                window.location.replace(APPLICATION_PATH);
-            }
-        } else {
+        if (localStorage.getItem("sessionId") && ACCOUNT_MANAGEMENT_PATHS.includes(window.location.href)) {
+            window.location.replace(APPLICATION_PATH);
+        } 
+        else if (!ACCOUNT_MANAGEMENT_PATHS.includes(window.location.href)) {
             window.location.replace(LOGIN_PATH);
+        }
+        else {
+            return;
         }
     }
 })();
