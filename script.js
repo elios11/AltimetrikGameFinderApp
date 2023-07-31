b374f752ef50936951a41db638fd5d03355165e3
elios11 <eliosferre3@hotmail.com>
2023-07-30 23:55:18 -0300

    Add global variables for modularization purposes

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+const APPLICATION_PATH = "http://localhost:5500/";
+const SERVER_PATH = "http://localhost:3000/";
+
+const LOGIN_PATH = `${APPLICATION_PATH}login.html`;
+const REGISTER_PATH = `${APPLICATION_PATH}register.html`;
+
+const API_KEY = "ce0e55ad125d4dabba067f72f6fcfc66";
+const API_PATH = "https://api.rawg.io/api/";
